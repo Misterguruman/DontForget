385ed9b10eb6ddef4f648ed45a56bf4b83334ab9
Joseph Langford <joseph.p.langford@gmail.com>
2024-05-02 11:53:56 -0400

    Add project files.

---
+++ DontForget/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DontForget
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DontForget/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DontForget
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
